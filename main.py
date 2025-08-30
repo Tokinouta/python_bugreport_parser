26b441a997d26398fc8f0a74cada1ae8ccfdbeb0
Boyao Hou <shangrila-1100@hotmail.com>
2025-08-30 21:35:02 +0800

    refactor(main): modify feedback processing to handle multiple IDs

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import tomllib
 import os
 
-from python_bugreport_parser.bugreport.bugreport_all import Log284
 from python_bugreport_parser.plugins import BugreportAnalysisContext, PluginRepo
 from python_bugreport_parser.download import download_one_file
 
@@ -13,22 +12,22 @@ with open(CONFIG_PATH, "rb") as f:
     config = tomllib.load(f)
 extract_path = Path(config["extract_path"])
 
-# feedback_id = "113280755"
-# feedback_dir = extract_path / str(feedback_id)
-# a = Log284.from_dir(feedback_dir)
-# a.bugreport.bugreport_txt.error_timestamp = datetime(2025, 4, 6, 15, 40, 0)
+feedback_ids = [
+    "115486261"
+]
+
 home_dir = os.getenv("HOME")
 if not home_dir:
     home_dir = "."
-user_feedback_path = os.path.join(home_dir, "jira", "UTUG-60303")
 
-context = BugreportAnalysisContext()
-context.bugreport = download_one_file("114302618", user_feedback_path)
-context.bugreport = Log284.from_dir(user_feedback_path)
-PluginRepo.run_all(context)
-with open(
-    "/home/dayong/workspace/others/code/python_bugreport_parser/output.txt",
-    "w",
-    encoding="utf-8",
-) as file:
-    file.write(PluginRepo.report_all())
+for id in feedback_ids:
+    user_feedback_path = os.path.join(home_dir, "jira", "OS3-feedback", id)
+    context = BugreportAnalysisContext()
+    context.bugreport = download_one_file(id, user_feedback_path)
+    PluginRepo.run_all(context)
+    with open(
+        "/home/dayong/workspace/others/code/python_bugreport_parser/output.txt",
+        "w",
+        encoding="utf-8",
+    ) as file:
+        file.write(PluginRepo.report_all())
